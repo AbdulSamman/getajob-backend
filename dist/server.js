881e9e9fb32ffce7da71995d3352cbe206d19bb1
Your Name <you@example.com>
2022-12-06 12:02:20 +0100

    backend finished

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,34 @@
-import { createServer } from "http";
-import { mainContent } from "./content.js"; //content.ts => fehlermeldung, vite/react löst das
+// import { createServer } from "http";
+// import { mainContent } from "./content.js"; //content.ts => fehlermeldung, vite/react löst das
+// //filesystem  z.b verzeichnisse lesen (festplatte)
+// import fs from "fs";
+// const port = 3115;
+// // JSON.parse => object   ohne parse => string
+// // express kummert sich um diese kleinigkeiten
+// const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+// console.log(jobs);
+// createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text-plain" });
+//   res.write(JSON.stringify(jobs));
+//   res.end();
+// }).listen(port);
+// console.log(`listening on http://localhost:${port}`);
+// express
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+const app = express();
+app.use(cors());
 const port = 3115;
-createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text-plain" });
-    res.write(mainContent);
-    res.end();
-}).listen(port);
-console.log(`listening on http://localhost:${port}`);
+const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+console.log(jobs);
+app.get("/", (req, res) => {
+    res.send("job site api");
+});
+app.get("/jobs", (req, res) => {
+    res.json(jobs);
+});
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,43 @@
-import { createServer } from "http";
-import { mainContent } from "./content.js"; //content.ts => fehlermeldung, vite/react löst das
+// import { createServer } from "http";
+// import { mainContent } from "./content.js"; //content.ts => fehlermeldung, vite/react löst das
+// //filesystem  z.b verzeichnisse lesen (festplatte)
+// import fs from "fs";
+
+// const port = 3115;
+// // JSON.parse => object   ohne parse => string
+// // express kummert sich um diese kleinigkeiten
+
+// const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+// console.log(jobs);
+
+// createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text-plain" });
+//   res.write(JSON.stringify(jobs));
+//   res.end();
+// }).listen(port);
+
+// console.log(`listening on http://localhost:${port}`);
+
+// express
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
 const port = 3115;
 
-createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text-plain" });
-  res.write(mainContent);
-  res.end();
-}).listen(port);
+const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json", "utf8"));
+console.log(jobs);
 
-console.log(`listening on http://localhost:${port}`);
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("job site api");
+});
+
+app.get("/jobs", (req: express.Request, res: express.Response) => {
+  res.json(jobs);
+});
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
